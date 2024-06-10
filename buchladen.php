d6284d8cd6d6ea103fa1d1908644d079543f2aa7
ByJuix <julian@toast24.de>
2024-06-10 10:21:30 +0200

    addednedit function

--- buchladen.php
+++ buchladen.php
@@ -46,7 +46,37 @@ switch ($_GET['action']) {
 
         break;
     case 'edit':
-        // Code for 'edit' action
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['table'])) {
+            $table = $_GET['table'];
+            $columns = $dbAdapter->db_query("SELECT COLUMN_NAME FROM information_schema.columns WHERE table_name = '{$table}'");
+            $updateColumns = [];
+            $updateValues = [];
+    
+            foreach ($columns as $column) {
+                $columnName = $column['COLUMN_NAME'];
+                if (isset($_POST[$columnName])) {
+                    $updateColumns[] = $columnName;
+                    $updateValues[] = $_POST[$columnName];
+                }
+            }
+    
+            if (!empty($updateColumns) && !empty($updateValues)) {
+                $updateColumnsString = implode(', ', $updateColumns);
+                $updateValuesString = implode(', ', array_map(function($value) {
+                    return "'{$value}'";
+                }, $updateValues));
+                $query = "UPDATE {$table} SET ";
+                for ($i = 0; $i < count($updateColumns); $i++) {
+                    $query .= "{$updateColumns[$i]} = '{$updateValues[$i]}'";
+                    if ($i < count($updateColumns) - 1) {
+                        $query .= ', ';
+                    }
+                }
+                $query .= "WHERE {$_GET['table']}_id = {$_GET['id']}";
+                $result = $dbAdapter->db_query($query);
+            }
+        }
+        header("Location: buchladen.php?table={$_GET['table']}");
         break;
     case 'delete':
         $result = $dbAdapter->db_query("DELETE FROM {$_GET['table']} WHERE {$_GET['table']}_id = {$_GET['id']}");
@@ -75,7 +105,7 @@ switch ($_GET['action']) {
             $result = $dbAdapter->db_query("SELECT * FROM {$_GET['table']}");
 
             // Beginne die Tabelle und füge die Überschriften hinzu.
-            echo "<div class='m-8'><table class='border-collapse table-auto w-full text-left'>";
+            echo "<div class='m-8'><table class='border-collapse table-fixed w-full text-left'>";
             if (!empty($result)) {
                 echo "<tr>";
                 foreach (array_keys($result[0]) as $columnName) {
@@ -103,14 +133,18 @@ switch ($_GET['action']) {
                             <a href='' class='text-yellow-500 edit-button'><i data-lucide='pencil'></i></a>
                         </td>
                     </tr>
-                    <tr class='hidden'>
-                        <td colspan='" . count($row) . "'>
-                            <form action='?table={$_GET['table']}&id={$firstCell}&action=edit' method='post'>
+                    <tr class='edit-row hidden'>
+                        <td colspan='" . count($row) + 1 . "'>
+                            <form action='?table={$_GET['table']}&id={$firstCell}&action=edit' method='post' class='flex flex-row'>
                                 ";
                                 foreach ($row as $columnName => $cell) {
-                                    echo "<input type='text' name='{$columnName}' value='{$cell}' class='px-4 py-2 bg-slate-200 rounded-full mr-4'>";
+                                    echo "<div class='input-field pr-4'><input type='text' name='{$columnName}' value='{$cell}' class='px-4 py-2 bg-slate-200 rounded-full w-full'></div>";
                                 }
-                                echo "<button class='text-green-500'><i data-lucide='check'></i></button>
+                                echo "
+                                <div>
+                                    <button type='submit' class='text-green-500 mr-4'><i data-lucide='check'></i></button>
+                                    <button class='text-red-500 cancel-edit-button'><i data-lucide='circle-x'></i></button>
+                                </div>
                             </form>
                         </td>
                     </tr>";                    
@@ -124,8 +158,37 @@ switch ($_GET['action']) {
                             event.preventDefault();
                             var editRow = this.parentNode.parentNode.nextElementSibling;
                             editRow.classList.remove('hidden');
+                            var parentRow = this.parentNode.parentNode;
+                            parentRow.classList.add('hidden');
                         });
                     });
+
+                    document.querySelectorAll('.cancel-edit-button').forEach(function(button) {
+                        button.addEventListener('click', function(event) {
+                            event.preventDefault();
+                            var editRow = this.parentNode.parentNode;
+                            editRow.classList.add('hidden');
+                            var originalRow = editRow.previousElementSibling;
+                            originalRow.classList.remove('hidden');
+                            var inputs = editRow.querySelectorAll('.input-field');
+                            inputs.forEach(function(input) {
+                                input.value = '';
+                            });
+                        });
+                    });
+
+                    window.onload = function() {
+                        var ths = document.querySelectorAll('th');
+                        var rows = document.querySelectorAll('.edit-row');
+                        rows.forEach(function(row) {
+                            var inputs = row.querySelectorAll('.input-field');
+                            for (var i = 0; i < ths.length; i++) {
+                                if (inputs[i]) {
+                                    inputs[i].style.width = ths[i].offsetWidth + 'px';
+                                }
+                            }
+                        });
+                    }
                 </script>
             HTML;
 
