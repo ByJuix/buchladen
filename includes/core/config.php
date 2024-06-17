154c31ff6a061e323293a744e26413fab3e4cdc9
ByJuix <julian@toast24.de>
2024-06-17 09:59:02 +0200

    chaged user to root (standard for xampp)

--- includes/core/config.php
+++ includes/core/config.php
@@ -3,5 +3,5 @@ const DB_TYPE = 'mysql';
 const DB_SERVER = 'localhost';
 const DB_PORT = '3306';
 const DB_NAME = 'buchladen';
-const DB_USER = 'user';
-const DB_PASSWORD = 'sicherheit123';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
